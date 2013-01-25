5f66cc016ff066b076709c0ae2ebb1dd181d95d1
Fernando Jascovich <fernando.ej@gmail.com>
2013-01-25 13:35:51 -0300

    remove sqlite3

--- test.py
+++ test.py
@@ -4,11 +4,9 @@
 # enable debugging
 import cgitb
 import sys
-import sqlite3
 
 cgitb.enable()
 
 print "Content-Type: text/plain;charset=utf-8"
 print
 print "python: \n%s\n" % sys.version
-print "sqllite: \n%s\n" % sqlite3.version
