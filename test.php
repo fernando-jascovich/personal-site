f008b7b4db29988054df30574de38795f237501b
Fernando Jascovich <fernando.ej@gmail.com>
2013-01-26 14:34:00 -0300

    change of plans

--- test.php
+++ test.php
@@ -1,7 +1,19 @@
 <?php
 
-echo '<pre>';
-print_r(apache_get_modules());
-echo '</pre>';
+	$service_url = 'cgi-bin/test.py';
+	$headers = array(
+			    	'Accept: application/json'
+				);
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $service_url);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+	$response = curl_exec($ch);
+
+	curl_close($ch);
+
+	var_dump($response);
 
 ?>
